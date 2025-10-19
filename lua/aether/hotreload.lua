e4dc8e4ee3c3fdf870c45971677878f6e5e7ce3d
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-10-19 18:43:14 +0200

    wait 2 seconds to reload

--- lua/aether/hotreload.lua
+++ lua/aether/hotreload.lua
@@ -5,8 +5,8 @@
 local M = {}
 
 -- Configuration constants
-local LAZY_RELOAD_DELAY_MS = 50
-local OMARCHY_RELOAD_DELAY_MS = 500
+local LAZY_RELOAD_DELAY_MS = 2000
+local OMARCHY_RELOAD_DELAY_MS = 2000
 local OMARCHY_THEME_PATH = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
 
 -- Patterns for module matching
